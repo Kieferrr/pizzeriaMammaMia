ec26175201deb15e3fcb398c81191576f0a8e7a7
Nicolás <nicog.rivero@gmail.com>
2025-06-21 23:06:56 -0400

    Agregar nombre del desarrollador en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div className="footer container-fluid">
             <p>© 2021 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
+            <p>Desarrollado por Nicolás Rivero Kiefer</p>
         </div>
     )
 }
