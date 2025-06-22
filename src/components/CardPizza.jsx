168bdc57e5b03d5ee8d72e54ca256fdee9f72e20
Nicolás <nicog.rivero@gmail.com>
2025-06-21 22:05:48 -0400

    Agregar el contenido del componente CardPizza

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,8 +1,22 @@
 
 
-const CardPizza = () => {
+const CardPizza = ({img, name, ingredients, price}) => {
     return (
-        <div>CardPizza</div>
+            <div className="card-pizza">
+                <img src={img} alt="" />
+                <h3 className="card-title">Pizza {name}</h3>
+                <div className="card-ingredients">
+                    <p className="ingredients-title">Ingredientes:</p>
+                    <p>🍕 {ingredients.join(", ")}</p>
+                </div>
+                <div className="card-price">
+                    <p className="price-title">Precio: ${price}<span></span></p>
+                    <div className="card-buttons">
+                        <button className="more-button">Ver Más 👀</button>
+                        <button className="add-button">Añadir 🛒</button>
+                    </div>
+                </div>
+            </div>
     )
 }
 
