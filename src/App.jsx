263df8a2df6b3c39ecbfd1dd444dd871d352e726
Nicolás <nicog.rivero@gmail.com>
2025-06-26 19:40:24 -0400

    Agregar contenido y estilos iniciales al RegisterPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import RegisterPage from './components/RegisterPage';
 
 function App() {
 
   return (
     <>
     <Navbar />
-    <Home />
+    {/* <Home /> */}
+    <RegisterPage />
     <Footer />
   </>
   )
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,7 +1,35 @@
+import './RegisterPage.css'
+import { useState } from 'react'
 
 const RegisterPage = () => {
+
     return (
-        <div>RegisterPage</div>
+        <div className="container register-page">
+                <div className='register-title'>
+                    <h2>Sign Up</h2>
+                </div>
+                <form className='register-form' action="">
+                    <p>Email</p>
+                    <input
+                        type="text"
+                        placeholder='Ingresa tu correo'
+                        className='register-input'
+                    />
+                    <p>Contraseña</p>
+                    <input
+                        type="text"
+                        placeholder='Ingresa tu contraseña'
+                        className='register-input'
+                    />
+                    <p>Confirmar contraseña</p>
+                    <input
+                        type="text"
+                        placeholder='Vuelve a ingresar tu contraseña'
+                        className='register-input'
+                    />
+                    <button className='register-button'>Registrar</button>
+                </form>
+        </div>
     )
 }
 
