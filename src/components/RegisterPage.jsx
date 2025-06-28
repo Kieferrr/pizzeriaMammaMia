1a2a6a62add8ec6f6c545ac1f6a11fc135d400a6
Nicolás <nicog.rivero@gmail.com>
2025-06-27 20:43:06 -0400

    Agregar useState para la validación. Agregar mejoras al diseño

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -3,34 +3,79 @@ import { useState } from 'react'
 
 const RegisterPage = () => {
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const trimmedEmail = email.trim();
+        const trimmedPassword = password.trim();
+        const trimmedConfirmPassword = confirmPassword.trim();
+
+        if (!trimmedEmail || !trimmedPassword || !trimmedConfirmPassword) {
+            alert("Ningún campo puede estar vacío o contener solo espacios");
+            return;
+        }
+        
+        if (trimmedPassword.length < 6) {
+            alert("La contraseña debe tener al menos 6 carácteres (sin contar espacios al inicio o final)");
+            return;
+        }
+
+        if (trimmedPassword !== trimmedConfirmPassword) {
+            alert("Las contraseñas no coinciden")
+            return;
+        }
+
+        setEmail("");
+        setPassword("")
+        setConfirmPassword("")
+        alert("Registro exitoso")
+    }
+
+
     return (
         <div className="container register-page">
-                <div className='register-title'>
-                    <h2>Sign Up</h2>
-                </div>
-                <form className='register-form' action="">
-                    <p>Email</p>
-                    <input
-                        type="text"
-                        placeholder='Ingresa tu correo'
-                        className='register-input'
-                    />
-                    <p>Contraseña</p>
-                    <input
-                        type="text"
-                        placeholder='Ingresa tu contraseña'
-                        className='register-input'
-                    />
-                    <p>Confirmar contraseña</p>
-                    <input
-                        type="text"
-                        placeholder='Vuelve a ingresar tu contraseña'
-                        className='register-input'
-                    />
-                    <button className='register-button'>Registrar</button>
-                </form>
+            <div className='register-title'>
+                <h2>Register</h2>
+            </div>
+            <form
+                className='register-form'
+                onSubmit={handleSubmit}
+            >
+                <label>Email</label>
+                <input
+                    type="email"
+                    placeholder='Ingresa tu correo'
+                    className='register-input'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <label>Contraseña</label>
+                <input
+                    type="password"
+                    placeholder='Ingresa tu contraseña'
+                    className='register-input'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <label>Confirmar contraseña</label>
+                <input
+                    type="password"
+                    placeholder='Vuelve a ingresar tu contraseña'
+                    className='register-input'
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button
+                    type='submit'
+                    className='register-button'
+                >Registrar
+                </button>
+            </form>
         </div>
     )
 }
-
 export default RegisterPage
