095ab0d4182e7823cdc98cdb60be1027ac8ddf62
Nicolás <nicog.rivero@gmail.com>
2025-07-03 16:05:33 -0400

    Comentar componentes que no se usarán

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import RegisterPage from './components/RegisterPage';
-import LoginPage from './components/LoginPage';
+// import RegisterPage from './components/RegisterPage';
+// import LoginPage from './components/LoginPage';
 
 function App() {
 
   return (
     <>
     <Navbar />
-    {/* <Home /> */}
+    <Home />
     {/* <RegisterPage /> */}
-    <LoginPage />
+    {/* <LoginPage /> */}
     <Footer />
   </>
   )
