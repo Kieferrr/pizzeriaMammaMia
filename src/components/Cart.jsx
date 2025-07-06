3803a215b0ed8cbc913dfcff68b3c7df014b9153
Nicolás <nicog.rivero@gmail.com>
2025-07-06 18:02:04 -0400

    Agregar estilos y template inicial de Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,8 +3,19 @@ import { useState } from "react"
 import './Cart.css'
 
 const Cart = () => {
+    
     return (
-        <div>Cart</div>
+        <div className="container cart-page">
+            <div className="container-fluid d-flex flex-column cart">
+                <p className="cart-title">Detalles del pedido:</p>
+                <div className="cart-products d-flex">
+                <p>Napolitana</p><p>$5.950</p>
+                <button className="minus-button">-</button> <p className="quantity">1</p> <button className="plus-button">+</button>
+                </div>
+                <p className="total">Total:</p>
+                <button className="pay-button">Pagar</button>
+            </div>
+        </div>
     )
 }
 
