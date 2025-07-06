560a0c1e7839ceeb5464986a3a9795f91294ecf9
Nicolás <nicog.rivero@gmail.com>
2025-07-06 18:21:40 -0400

    Renderizar la información con el array de pizzaCart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,22 @@
 import { pizzaCart } from "../data/pizzas"
 import { useState } from "react"
+import { formatPrice } from "../utils/formatPrice"
 import './Cart.css'
 
 const Cart = () => {
-    
+
+    const [cart, setCart] = useState(pizzaCart)
+
     return (
         <div className="container cart-page">
-            <div className="container-fluid d-flex flex-column cart">
+                <div className="container-fluid d-flex flex-column cart">
                 <p className="cart-title">Detalles del pedido:</p>
-                <div className="cart-products d-flex">
-                <p>Napolitana</p><p>$5.950</p>
-                <button className="minus-button">-</button> <p className="quantity">1</p> <button className="plus-button">+</button>
+            {cart.map((pizza) => (
+                <div key={pizza.id} className="cart-products d-flex">
+                    <img src={pizza.img} className="cart-img" /><p>{pizza.name}</p><p>{formatPrice(pizza.price)}</p>
+                    <button className="minus-button">-</button> <p className="quantity">{pizza.count}</p> <button className="plus-button">+</button>
                 </div>
+            ))}
                 <p className="total">Total:</p>
                 <button className="pay-button">Pagar</button>
             </div>
