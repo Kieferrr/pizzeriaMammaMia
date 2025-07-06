4ef875928dccd7839c9755096dd7a157fe031ccb
Nicolás <nicog.rivero@gmail.com>
2025-07-06 17:05:02 -0400

    Añadir componente Cart.jsx y su css

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import { pizzaCart } from "../data/pizzas"
+import { useState } from "react"
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div>Cart</div>
+    )
+}
+
+export default Cart
