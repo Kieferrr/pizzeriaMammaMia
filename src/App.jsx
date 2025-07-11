7f6d8bcc159f1c7b4f5bc28a023c85bf140a4e0a
Nicolás <nicog.rivero@gmail.com>
2025-07-11 19:40:41 -0400

    Agregar el componente Pizza. Cambiar lo que se muestra desde App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+// import Home from './components/Home';
 import Footer from './components/Footer';
 // import RegisterPage from './components/RegisterPage';
 // import LoginPage from './components/LoginPage';
 // import Cart from './components/Cart';
+import Pizza from './components/Pizza'
 
 function App() {
 
   return (
     <>
     <Navbar />
-    <Home />
+    {/* <Home /> */}
     {/* <RegisterPage /> */}
     {/* <LoginPage /> */}
     {/* <Cart /> */}
+    <Pizza />
     <Footer />
   </>
   )
---
+++ src/components/Pizza.jsx
@@ -0,0 +1,9 @@
+import './Pizza.css'
+
+const Pizza = () => {
+    return (
+        <div>Pizza</div>
+    )
+}
+
+export default Pizza
