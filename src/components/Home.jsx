b8281d056da3fef3c625516dde606662ac53da08
Nicolás <nicog.rivero@gmail.com>
2025-07-12 14:36:16 -0400

    Añadir loading a Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,18 @@ import { fetchData } from "../services/api"
 
 const Home = () => {
 
-    const [pizzas, setPizzas] = useState([])      
-    
+    const [pizzas, setPizzas] = useState([])
+
     const url = "http://localhost:5000/api/pizzas";
 
     useEffect(() => {
         fetchData(url).then(setPizzas).catch(console.error)
     }, [])
-    
+
+    if (pizzas.length === 0) {
+        return <p>Cargando pizzas...</p>;
+    }
+
     return (
         <div>
             <Header />
@@ -20,13 +24,13 @@ const Home = () => {
                 <div className="pizza-container row row-cols-1 row-cols-md-3">
                     {pizzas.map(p => (
                         <CardPizza
-                            key = {p.id}
-                            name= {p.name}
-                            price = {p.price}
-                            ingredients = {p.ingredients}
-                            img = {p.img}
-                        /> 
-                ))}
+                            key={p.id}
+                            name={p.name}
+                            price={p.price}
+                            ingredients={p.ingredients}
+                            img={p.img}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
