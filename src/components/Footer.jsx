f774ec084e7df47669ce4f5ff246aa6141e4b029
Nicolás <nicog.rivero@gmail.com>
2025-07-11 23:20:40 -0400

    Cambiar fecha de desarrollo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer container-fluid">
-            <p>© 2021 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
+            <p>© 2025 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
             <p>Desarrollado por Nicolás Rivero Kiefer</p>
         </div>
     )
