be1a7d4b094d143f3d0880aff72bf8a4b05e2b3a
Nicolás <nicog.rivero@gmail.com>
2025-07-11 22:31:04 -0400

    Agregar el componente Pizza renderizado con la API

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,8 +1,57 @@
+import { useEffect, useState } from 'react'
 import './Pizza.css'
+import axios from 'axios';
+import { formatPrice } from '../utils/formatPrice';
+import { capitalizeWords } from '../utils/capitalizeWords';
 
 const Pizza = () => {
+
+    const [pizza, setPizza] = useState(null);
+
+    const urlPizza1 = "http://localhost:5000/api/pizzas/p001"
+
+    const getData = async () => {
+        try {
+            const response = await axios.get(urlPizza1)
+            setPizza(response.data)
+            console.log(response.data);
+        } catch (error) {
+            console.log("Error en la petición:", error)
+        }
+
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    if (!pizza) {
+        return <p>Cargando Pizza...</p>
+    }
+
     return (
-        <div>Pizza</div>
+
+        <div className='desc-page'>
+            <div className='container desc-card'>
+                <img src={pizza.img} alt="" />
+                <div className='card-content'>
+                    <div className='card-head'>
+                    <h2 className='pizza-name'>{capitalizeWords(pizza.name)}</h2>
+                    <span className='price'>{formatPrice(pizza.price)}</span>
+                    </div>
+                    <h3 className='card-subtitle'>Descripción</h3>
+                    <p className='pizza-content'>{pizza.desc}</p>
+                    <h3 className='card-subtitle'>Ingredientes</h3>
+                    <div className='card-ing'>
+                        {pizza.ingredients.map((ing, index) => (
+                            <span key={index}>{capitalizeWords(ing)} </span>
+                        ))}
+                    </div>
+                    <button className='cart-button'>Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
