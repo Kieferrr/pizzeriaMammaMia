2d7356dd5b97e7ffb3830331200dc4258e47be09
Nicolás <nicog.rivero@gmail.com>
2025-07-11 23:26:27 -0400

    Cambiar nombre para no repetir

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ import axios from "axios"
 
 const Home = () => {
 
-    const [pizza, setPizza] = useState([])      
+    const [pizzas, setPizzas] = useState([])      
     
     const url = "http://localhost:5000/api/pizzas";
 
     const getData = async()  => {
         try {
             const { data } = await axios.get(url);
-            setPizza(data)
+            setPizzas(data)
         } catch (error) {
             console.error("Error en la petición:", error)
         }
@@ -27,7 +27,7 @@ const Home = () => {
             <Header />
             <div className="container-fluid">
                 <div className="pizza-container row row-cols-1 row-cols-md-3">
-                    {pizza.map(p => (
+                    {pizzas.map(p => (
                         <CardPizza
                             key = {p.id}
                             name= {p.name}
